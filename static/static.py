eccd48604f8a8b8eb4eb7ffe6baafb9085345861
maklore74 <63496781+maklore74@users.noreply.github.com>
2025-10-05 17:57:01 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -4,7 +4,7 @@ Cause all I want is to be loved
 And all I care about is you
 You’re stuck on me like a tattoo
 No, I don’t care about the pain
-I’ll walk through fire and through rain
+I’ll walk through fire and through rain """""""
 """
 <!DOCTYPE html>
 <html>
