cf9e80ba79e432506d24848f8af7ee7c4145ebfd
maklore74 <63496781+maklore74@users.noreply.github.com>
2025-10-05 18:35:17 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -3,7 +3,7 @@ Don’t care about them all
 Cause all I want is to be loved
 And all I care about is you
 You’re stuck on me like a tattoo
-No, I don’t care about the pain
+No I don’t care about the pain
 I’ll walk through fire and through rain
 """"
 <!DOCTYPE html>
